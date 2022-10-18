b259e8051e48714b0b781570d01a0d888a6d10d2
benadepoju <sunnybenpoju@gmail.com>
2022-10-18 16:39:46 -0700

    new

--- print_hexadecimal_low.c
+++ print_hexadecimal_low.c
@@ -3,7 +3,7 @@
  * print_hexadecimal_low - Print a number in hexadecimal format
  * @list: Number to print
  * Return: Length of the number
-*/
+ */
 int print_hexadecimal_low(va_list list)
 {
 char *p_buff;
